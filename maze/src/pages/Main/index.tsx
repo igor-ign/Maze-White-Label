da848f2c062ec142641c7a593058f00b1a1983d9
Igor Ignácio <igor.ignacio@hypera.com.br>
2023-11-19 16:37:10 -0300

    feat: header component

--- maze/src/pages/Main/index.tsx
+++ maze/src/pages/Main/index.tsx
@@ -1,21 +1,8 @@
-import { IMAGES } from "../../assets/images";
-import { BRAND_DISPLAY_NAME } from "../../constants";
+import { Header } from "../../components";
 import './style.scss'
 
 export function MainPage() {
     return <main className="main-page-container">
-        <header className="main-page-header">
-            <img src={IMAGES.LOGO} alt={`${BRAND_DISPLAY_NAME} logo`} className="header-logo"/>
-
-            <nav className="navigation-container">
-                <ul className="navigation-links-list">
-                    <li className="navigation-item">Home</li>
-                    <li className="navigation-item">{BRAND_DISPLAY_NAME === "Maze Cars" ? "Cars" : "Motorcycles"}</li>
-                    <li className="navigation-item">Search</li>
-                    <li className="navigation-item">Contact</li>
-                    <li className="navigation-item">About</li>
-                </ul>
-            </nav>
-        </header>
+        <Header />
     </main>
 }
