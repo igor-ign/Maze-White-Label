1e17334c0187cd990dbd5803e33bb49baee49737
Igor Ignácio <igor.ignacio@hypera.com.br>
2023-11-20 10:24:44 -0300

    feat: main page maze car banner

--- maze/src/pages/Main/index.tsx
+++ maze/src/pages/Main/index.tsx
@@ -1,8 +1,25 @@
+import { IMAGES } from "../../assets/images";
 import { Header } from "../../components";
+import { BRAND_DISPLAY_NAME } from "../../constants";
 import './style.scss'
 
 export function MainPage() {
+    const bannerTitleParagraphAndImageAltKeyword = BRAND_DISPLAY_NAME === "Maze Cars" ? "car" : "motorcycle" 
+
     return <main className="main-page-container">
             <Header /> 
+
+            <article className="page-content">
+                <section className="page-banner">
+                    <div className="banner-title-container">
+                        <h1 className="banner-title">Search, find and buy
+                        the ideal {bannerTitleParagraphAndImageAltKeyword} easily.</h1>
+                        <p className="banner-paragraph">The easiest and safest way of buying 
+                        your dream {bannerTitleParagraphAndImageAltKeyword}.</p>
+                    </div>
+
+                    <img src={IMAGES.BANNER_IMAGE} alt={`${BRAND_DISPLAY_NAME} banner. White ${bannerTitleParagraphAndImageAltKeyword} without background.`} className="banner-image" />
+                </section>
+            </article>
     </main>
 }
