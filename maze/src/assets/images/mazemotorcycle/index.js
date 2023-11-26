00fbad2ceca40c480757ea9bd011693e5c855f1d
Igor Ignácio <igor.ignacio@hypera.com.br>
2023-11-26 11:49:02 -0300

    feat: trusted motorcycle brand images

--- maze/src/assets/images/mazemotorcycle/index.js
+++ maze/src/assets/images/mazemotorcycle/index.js
@@ -1,7 +1,39 @@
 import Logo from './logo.png'
 import BannerImage from './banner.png'
+import BmwLogo from './bmw.png'
+import DucattiLogo from './ducatti.png'
+import HondaLogo from './honda.png'
+import KawasakiLogo from './kawasaki.png'
+import SuzukiLogo from './suzuki.png'
+import YamahaLogo from './yamaha.png'
 
 export const IMAGES = {
     LOGO: Logo,
     BANNER_IMAGE: BannerImage,
+    TRUSTED_BRANDS: [
+        {
+            alt: "BMW Logo",
+            image: BmwLogo
+        },
+        {
+            alt: "Ducatti Logo",
+            image: DucattiLogo
+        },
+        {
+            alt: "Honda Logo",
+            image: HondaLogo
+        },
+        {
+            alt: "Kawasaki Logo",
+            image: KawasakiLogo
+        },
+        {
+            alt: "Suzuki Logo",
+            image: SuzukiLogo
+        },
+        {
+            alt: "Yamaha Logo",
+            image: YamahaLogo
+        }
+    ]
 }
