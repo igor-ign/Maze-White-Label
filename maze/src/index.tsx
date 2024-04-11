1a5b1796b2286072480b4b8e1f7af29e50927d50
Igor Ignácio <igor.ignacio@hypera.com.br>
2024-04-11 17:37:01 -0300

    feat: added dynamic page title

--- maze/src/index.tsx
+++ maze/src/index.tsx
@@ -2,12 +2,13 @@ import ReactDOM from 'react-dom/client';
 import {
   RouterProvider,
 } from "react-router-dom";
-import { APP_ROUTES } from './constants';
+import { APP_ROUTES, BRAND_DISPLAY_NAME } from './constants';
 import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+document.title = BRAND_DISPLAY_NAME
 root.render(
     <RouterProvider router={APP_ROUTES}/>
 );
